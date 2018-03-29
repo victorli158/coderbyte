b07eb11851f6335432ad0b08b366613c2187daa3
Victor Li <victor.li158@gmail.com>
2018-03-28 18:53:49 -0700

    Revise rectangleArea

--- rectangle_area.js
+++ rectangle_area.js
@@ -1,15 +1,21 @@
-function rectangleArea(strArr) { 
-  let xCoordinates = [];
-  let yCoordinates = [];
+function rectangleArea(strArr) {
+  let xCoords = [];
+  let yCoords = [];
+  let current;
   for (let i = 0; i < strArr.length; i++) {
-    if (!xCoordinates.includes(strArr[i][1])) {
-      xCoordinates.push(strArr[i][1]);
+    current = strArr[i].split(' ');
+    current[0] = current[0].slice(1);
+    if (current[1].length === 3) {
+      current[1] = current[1].slice(0, 2);
+    } else {
+      current[1] = current[1].slice(0, 1);
     }
-    if (!yCoordinates.includes(strArr[i][3])) {
-      yCoordinates.push(strArr[i][3]);
-    }
+    xCoords.push(parseInt(current[0]));
+    yCoords.push(parseInt(current[1]));
   }
-  let width = Math.abs(parseInt(xCoordinates[0]) - Math.abs(parseInt(xCoordinates[1])));
-  let height = Math.abs(parseInt(yCoordinates[0]) - Math.abs(parseInt(yCoordinates[1])));
-  return width * height;
+  xCoords.sort(function(a, b) { return a - b; });
+  yCoords.sort(function(a, b) { return a - b; });
+  let length = xCoords[3] - xCoords[0];
+  let height = yCoords[3] - yCoords[0];
+  return length * height;
 }
